52f6ba8ceb4a1450648b5df245a15f38e2168088
Brian Keetman <info@briankeetman.nl>
2023-07-13 14:55:11 +0200

    bg color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
       <Image
         alt="Logo"
         src={'/logo-briankeetman-nl.png'}
