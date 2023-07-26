1e12b24345d2b66ce691b1abedd241888e2fe52b
Brian Keetman <info@briankeetman.nl>
2023-07-26 12:08:10 +0200

    Adding more info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,20 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-800">
       <Image alt="Logo" src={Logo} width={200} height={200} />
       <div className="text-center text-slate-100">
-        <div className="text-left inline-block">
+        <div className="text-left inline-block prose dark:prose-invert">
+          <h3>briankeetman.nl</h3>
           <p>
-            <b>briankeetman.nl</b>
+            Zuidwesterringweg 34a
+            <br />
+            8309PH Tollebeek
+            <br />
+            info@briankeetman.nl
           </p>
-          <p>Zuidwesterringweg 34a</p>
-          <p>8309 PH Tollebeek</p>
-          <p>info@briankeetman.nl</p>
+          <p className="prose-sm">
+            KVK: 90764048
+            <br />
+            BTW: NL004164420B26
+          </p>
         </div>
       </div>
     </main>
