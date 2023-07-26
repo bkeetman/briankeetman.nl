d07627cc40d12ad506683a87bb3437c2730fe5ee
Brian Keetman <info@briankeetman.nl>
2023-07-26 12:21:43 +0200

    Update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,25 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-800">
       <Image alt="Logo" src={Logo} width={200} height={200} />
-      <div className="text-center text-slate-100">
-        <div className="text-left inline-block prose dark:prose-invert">
-          <h3>briankeetman.nl</h3>
-          <p>
-            Zuidwesterringweg 34a
-            <br />
-            8309PH Tollebeek
-            <br />
-            info@briankeetman.nl
-          </p>
-          <p className="prose-sm">
-            KVK: 90764048
-            <br />
-            BTW: NL004164420B26
-          </p>
+      <div className="text-center text-slate-100 prose dark:prose-invert">
+        <h2 className="text-left  mb-4">briankeetman.nl</h2>
+        <div className="flex justify-center items-start">
+          <div className="text-left inline-block">
+            <p>
+              Zuidwesterringweg 34a
+              <br />
+              8309PH Tollebeek
+              <br />
+              info@briankeetman.nl
+            </p>
+          </div>
+          <div className="text-left inline-block ml-8">
+            <p className="prose-sm">
+              KVK: 90764048
+              <br />
+              BTW: NL004164420B26
+            </p>
+          </div>
         </div>
       </div>
     </main>
