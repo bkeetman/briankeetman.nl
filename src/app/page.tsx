e8fafd6013427b1799e68912a4e61512a8a42233
Brian Keetman <info@briankeetman.nl>
2023-07-26 11:12:46 +0200

    Update logo and text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import Image from 'next/image';
+import Logo from '../../public/logo-briankeetman-nl.svg';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-300">
-      <Image
-        alt="Logo"
-        src={'/logo-briankeetman-nl.png'}
-        width={200}
-        height={200}
-      />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-800">
+      <Image alt="Logo" src={Logo} width={200} height={200} />
+      <div className="text-center text-slate-100">
+        <div className="text-left inline-block">
+          <p>
+            <b>briankeetman.nl</b>
+          </p>
+          <p>Zuidwesterringweg 34A</p>
+          <p>8309 PH Tollebeek</p>
+          <p>info@briankeetman.nl</p>
+        </div>
+      </div>
     </main>
   );
 }
