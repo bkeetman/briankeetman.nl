9e0645383f56dbd35248adfa1f96b2ef0c758cc3
Brian Keetman <info@briankeetman.nl>
2024-12-27 18:29:32 +0100

    Enhance homepage layout and text alignment for improved presentation
    - Centered the logo and adjusted the layout for better visual balance.
    - Updated text alignment to center for improved readability.
    - Consolidated address and company information into a more organized format.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,30 @@ export default function Home() {
   return (
     <main className="flex min-h-screen">
       {/* Left half */}
-      <div className="w-1/2 p-8 flex flex-col items-center">
+      <div className="w-1/2 p-8 flex flex-col items-center justify-center">
         {/* Logo centered */}
         <div className="mb-12">
           <Image alt="Logo" src={Logo} width={200} height={200} />
         </div>
 
         {/* Text content */}
-        <div className="text-slate-100 w-full max-w-md">
+        <div className="text-slate-100 w-full max-w-md text-center">
           <h2 className="mb-8 text-2xl font-semibold">briankeetman.nl</h2>
 
-          <div className="flex justify-between">
-            <div>
-              <p className="text-base leading-relaxed">
-                Zuidwesterringweg 34a
-                <br />
-                8309PH Tollebeek
-                <br />
-                info@briankeetman.nl
-              </p>
-            </div>
-            <div>
-              <p className="text-sm leading-relaxed">
-                KVK: 90764048
-                <br />
-                BTW: NL004164420B26
-              </p>
-            </div>
+          <div className="space-y-8">
+            <p className="text-base leading-relaxed">
+              Zuidwesterringweg 34a
+              <br />
+              8309PH Tollebeek
+              <br />
+              info@briankeetman.nl
+            </p>
+
+            <p className="text-sm leading-relaxed">
+              KVK: 90764048
+              <br />
+              BTW: NL004164420B26
+            </p>
           </div>
         </div>
       </div>
