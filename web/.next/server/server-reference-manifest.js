440a5f5ca7f3a3dad51b8b9909f7e6342fa1695f
Brian Keetman <info@briankeetman.nl>
2026-01-02 11:58:02 +0100

    Clean working tree without build artifacts

--- web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{\"7ffb3b029848584f4d7c924dd6bec494b41b37a5ff\":{\"workers\":{\"app/blog/page\":{\"moduleId\":\"95890\",\"async\":false}},\"layer\":{\"app/blog/page\":\"action-browser\"}}},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
